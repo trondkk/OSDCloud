f804e7965a1b068cef97ec4bfec41c9c687047e3
trondkk <36925170+trondkk@users.noreply.github.com>
2022-08-17 13:54:13 +0200

    Update ExampleScript.ps1

--- ExampleScript.ps1
+++ ExampleScript.ps1
@@ -1,14 +1,14 @@
-Write-Host -ForegroundColor Green “Starting OSDCloud ZTI”
+Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 Start-Sleep -Seconds 5
 
-if ((Get-MyComputerModel) -match ‘Virtual’) {
-Write-Host -ForegroundColor Green “Setting Display Resolution to 1600x”
+if ((Get-MyComputerModel) -match 'Virtual') {
+Write-Host -ForegroundColor Green "Setting Display Resolution to 1600x"
 Set-DisRes 1600
 }
 
-Write-Host -ForegroundColor Green “Start OSDCloud”
+Write-Host -ForegroundColor Green "Start OSDCloud"
 Start-OSDCloud -OSLanguage nb-no -OSBuild 21H2 -OSEdition Enterprise -ZTI
 
-Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 Start-Sleep -Seconds 20
 wpeutil reboot
